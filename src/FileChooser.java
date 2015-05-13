99ad80ff1aca32d5e96f2ea15871b2c385cada34
Daniel V. Gurevich <daniel@fuzzy.den.paulidav.org>
2015-05-13 13:56:06 -0700

    second commit

---
+++ src/FileChooser.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+
+public class FileChooser {
+	
+	private File f;
+	
+	public FileChooser() {
+		JFileChooser chooser = new JFileChooser();
+	    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+	        "MP3 and WAV Sound", "mp3", "wav");
+	    chooser.setFileFilter(filter);
+	    int returnVal = chooser.showOpenDialog(null);
+	    if(returnVal == JFileChooser.APPROVE_OPTION) {
+	       System.out.println("You chose to open this file: " +
+	            chooser.getSelectedFile().getName());
+	       f = chooser.getSelectedFile();
+	    }
+	}
+	
+	public File getFile() {
+		return f;
+	}
+}
