077b80160af2e7d9c3a415d5656f74522f58db00
Daniel V. Gurevich <daniel@fuzzy.den.paulidav.org>
2015-05-24 12:42:52 -0700

    3D level

--- src/Sketch02.java
+++ src/Sketch02.java
@@ -1,80 +1,133 @@
 import java.io.File;
+
 import processing.core.*;
+import processing.event.MouseEvent;
 import ddf.minim.*;
 import ddf.minim.analysis.*;
 
 public class Sketch02 extends PApplet {
-	Minim minim;
-	AudioPlayer song;
-	FFT fft;
 	static final long serialVersionUID = 1;
-	File clip;
+	private Minim minim;
+	private AudioPlayer song;
+	private FFT fft;
+	private File clip;
+	
+	private float cameraDist = 200;
+	private float scrollingSpeed = 20;
+	private float minAX = radians(90);
+	private float maxAX = radians(-90);
+	private float minAY = radians(0);
+	private float maxAY = radians(-90);
+	
+	private float maxLevel = 0;
 	 
-	public void setup()
-	{
-	  size(600, 400);
-	  frameRate(24);
-	  // always start Minim first!
-	  minim = new Minim(this);
-	 
-	  // specify 512 for the length of the sample buffers
-	  // the default buffer size is 1024
-	  FileChooser c = new FileChooser();
-	  clip = c.getFile();
-	  song = minim.loadFile(clip.getAbsolutePath(), 512);
-	  song.play();
-	 
-	  // an FFT needs to know how 
-	  // long the audio buffers it will be analyzing are
-	  // and also needs to know 
-	  // the sample rate of the audio it is analyzing
-	  fft = new FFT(song.bufferSize(), song.sampleRate());
+	public void setup() {
+		size(600, 600, P3D);
+		frameRate(60);
+		// always start Minim first!
+		minim = new Minim(this);
+/*		
+		// specify 512 for the length of the sample buffers
+		// the default buffer size is 1024
+		FileChooser c = new FileChooser();
+		clip = c.getFile();
+
+		//song = minim.loadFile(clip.getAbsolutePath(), 1024/2);
+		song = minim.loadFile(clip.getAbsolutePath(), 512);
+*/
+		song = minim.loadFile("/home/daniel/Music/tristam/Drumstep_-_Tristam_Braken_-_Flight_Monstercat_Release.mp3", 512);
+		song.play();
+		
+		// an FFT needs to know how 
+		// long the audio buffers it will be analyzing are
+		// and also needs to know 
+		// the sample rate of the audio it is analyzing
+		fft = new FFT(song.bufferSize(), song.sampleRate());
 	}
+	
+	public void mouseWheel(MouseEvent event) {
+		  cameraDist += event.getCount()*scrollingSpeed;
+		}
 	 
-	public void draw()
-	{
-	  background(0);
-	  // first perform a forward fft on one of song's buffers
-	  // I'm using the mix buffer
-	  //  but you can use any one you like
-	  fft.forward(song.mix);
-	 
-	  stroke(255, 0, 0, 128);
-	  strokeWeight(5);
-	  // draw the spectrum as a series of vertical lines
-	  // I multiple the value of getBand by 4 
-	  // so that we can see the lines better
-	  for(int i = 0; i < fft.specSize(); i++) {
-		  if (fft.getBand(i)*20 > height/4) {
-			  line(i*6, height/2, i*6, height/4);
-		  } else {
-			  line(i*6, height/2, i*6, height/2 - fft.getBand(i)*20 - 20);
-		  }
-	    //line(i*6, height/2, i*6, height/2 - fft.getBand(i)*20 - 20);
-	    //rect(i*5, height/3, 5, height - fft.getBand(i)*100);
-	  }
-	 
-	  stroke(255);
-	  strokeWeight(2);
-	  // I draw the waveform by connecting 
-	  // neighbor values with a line. I multiply 
-	  // each of the values by 50 
-	  // because the values in the buffers are normalized
-	  // this means that they have values between -1 and 1. 
-	  // If we don't scale them up our waveform 
-	  // will look more or less like a straight line.
-	  float step = width/(song.mix.size()-1);
-	  for(int i = 0; i < song.mix.size() - 1; i++)
-	  {
-	    line(i*step, height*3/4 + song.mix.get(i)*50, (i+1)*step, height*3/4 + song.mix.get(i+1)*50);
-	  }
+	public void draw() {
+		background(0);
+		fft.forward(song.mix);
+		setupCamera();
+		level3d();
+		
+		//simpleHist();
+		//simpleWave();
+		//amplitude();
+	}
+	
+	private void setupCamera() {
+		PVector cameraPosition = cameraPos();
+		camera(cameraPosition.x, cameraPosition.y, cameraPosition.z,
+				0, 0, 0,
+				0, 1, 0);
+	}
+	
+	private PVector spheric2Rect(float radius, float alpha, float beta) {
+		return new PVector(
+				radius*cos(alpha)*sin(beta),
+				radius*sin(alpha),
+				radius*cos(alpha)*cos(beta));
+	}
+	
+	private PVector cameraPos() {
+		return spheric2Rect(
+				cameraDist, 
+				map(mouseY, 0, height, minAY, maxAY), 
+				map(mouseX, 0, width, minAX, maxAX));
+	}
+	
+	private void level3d() {
+		float level = song.mix.level();
+		if (level > maxLevel) {
+			maxLevel = level;
+		} else {
+			maxLevel*=.99;
+		}
+		noStroke();
+		lights();
+		fill(153);
+		float boxHeight = level*(float)levelScale();
+		translate(0, -boxHeight/2, 0);
+		box(90, boxHeight, 90);
+		translate(0, boxHeight/2, 0);
+		translate(0, -maxLevel*(float)levelScale(), 0);
+		box(90, 5, 90);
+		text("maxLevel= " + maxLevel, 100, 0, 0);
+	}
+	
+	private double levelScale() {
+		return (double)height * 0.45;
+	}
+	
+	private void simpleHist() {
+		stroke(255, 0, 0, 128);
+		strokeWeight(5);
+		for(int i = 0; i < fft.specSize(); i++) {
+			line(i*6, height/2, i*6, height/2 - fft.getBand(i)*20 - 20);
+		}
+	}
+	
+	private void amplitude() {
+		stroke(0, 255, 0, 128);
+		strokeWeight(25);
+		line(10, height*7/8, width*song.mix.level()+10, height*7/8);
+	}
+	
+	private void simpleWave() {
+		stroke(255);
+		strokeWeight(2);
+		float[] samples = song.mix.toArray();
+		float step = (float)width/(float)(samples.length-1);
+		for(int i = 0; i < samples.length - 1; i++) {
+			line(i*step, height*3/4 + samples[i]*50, (i+1)*step, height*3/4 + samples[i+1]*50);
+		}
 	}
 
-	//public Sketch01(File f) {
-		//clip = f;
-		//PApplet.main(new String[] {"--present", "Sketch01"});
-	//}
-	
 	public static void main(String args[]) {
 		PApplet.main(new String[] { "--present", "Sketch01" });
 	}
